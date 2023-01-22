f2c3504138bd3a4daf40779f3b1e2e65069eb4f8
Popov <RusHollow@gmail.com>
2023-01-22 19:33:08 +1000

    Created main.py file. Added class TelBot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # first_telegram_bot
 we two are trying to find out how to work on git together
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+
+class TelBot:
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+if __name__ == "__main__":
+    bot = TelBot()
+    bot.start()
