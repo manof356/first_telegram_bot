5003b528d748fb0527e03e3545c9fbd932c4c9ac
Popov <RusHollow@gmail.com>
2023-02-05 12:35:14 +1000

    nothing

--- t_bot.py
+++ t_bot.py
@@ -8,6 +8,6 @@ dp = Dispatcher(bot)
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(text=message.text)
-
+    await message.answer(text="сам пидор")
 
 executor.start_polling(dp)
